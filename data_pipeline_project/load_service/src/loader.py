c253486747f08ba636fce5cef81255f8b1dff4a4
MarioCipriano423 <mario.cipriano423@gmail.com>
2026-03-03 13:57:40 -0800

    main. Version: 0.0.2, iteration: 1, 03MAR2026. 'load.py example'

--- data_pipeline_project/load_service/src/loader.py
+++ data_pipeline_project/load_service/src/loader.py
@@ -1,5 +1,22 @@
 '''
 '''
 
-def load_file(**kwargs):
-    pass
+import uuid
+import os
+from fastapi import UploadFile
+
+DATA_DIR = "datasets"
+os.makedirs(DATA_DIR, exist_ok=True)
+
+async def load_file(file: UploadFile):
+    dataset_id = str(uuid.uuid4())
+    file_extension = file.filename.split(".")[-1]
+    safe_filename = f"{dataset_id}.{file_extension}"
+
+    file_path = os.path.join(DATA_DIR, safe_filename)
+
+    with open(file_path, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
+    
+    return dataset_id
