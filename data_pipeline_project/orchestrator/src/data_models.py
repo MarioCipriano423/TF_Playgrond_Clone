a49f066103083793ace2822244d42dd4c6c5d949
MarioCipriano423 <mario.cipriano423@gmail.com>
2026-04-27 15:31:15 -0700

    main-V_0.0.12-27APR2026-'Making data models...'

---
+++ data_pipeline_project/orchestrator/src/data_models.py
@@ -0,0 +1,7 @@
+# pyright: reportMissingImports=false
+# pyright: reportMissingModuleSource=false
+
+from pydantic import BaseModel, Field 
+from typing import List, Literal, Optional
+
+# In process... 
