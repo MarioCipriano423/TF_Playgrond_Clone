dd7a4e99043512cb6fce538e99f77c303db39350
MarioCipriano423 <mario.cipriano423@gmail.com>
2026-04-28 15:32:11 -0700

    main-V_0.0.13-28APR2026-'Making data models: DatasetConfig...'

--- data_pipeline_project/orchestrator/src/data_models.py
+++ data_pipeline_project/orchestrator/src/data_models.py
@@ -4,4 +4,9 @@
 from pydantic import BaseModel, Field 
 from typing import List, Literal, Optional
 
+class DatasetConfig(BaseModel):
+    type: Literal["iris", "sintenic"]
+    size: float = Field(ge=50, le=100)
+    train_test_split: float = Field(ge=0.1, le=0.9)
+
 # In process... 
